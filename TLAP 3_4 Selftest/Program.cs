82374414d86644432725b373c6e474f4040ccfd4
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-14 14:28:16 +0200

    x
    x

--- TLAP 3_4 Selftest/Program.cs
+++ TLAP 3_4 Selftest/Program.cs
@@ -11,13 +11,14 @@ namespace TLAP_3_4_Selftest
         {
             var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
             var code = "QWERTYUIOPÅÆØLKJHGFDSAZXCVBNM";
-            var ciphertext = "Hei";
-            Encrypt(alphabet, code, ciphertext);
+            var ciphertext = "Hei på deg!";
+            MainEncrypt(alphabet, code, ciphertext);
+            //BackToPlainText(alphabet, code, ciphertext);
             Console.WriteLine(ciphertext);
-            var x = "ye";
+
         }
-      
-        static void Encrypt(string alphabet, string code, string ciphertext)
+
+        static void MainEncrypt(string alphabet, string code, string ciphertext)
         {
             var codeToArray = code.ToCharArray();
             var alphabetToArray = alphabet.ToCharArray();
@@ -31,10 +32,31 @@ namespace TLAP_3_4_Selftest
                     outputText += alphabetToArray[Array.IndexOf(codeToArray, i)];
                 }
                 else outputText += i;
-                //Console.WriteLine(i);
             }
 
             Console.WriteLine(outputText);
         }
+
+
+
+        //static void BackToPlainText(string alphabet, string code, string ciphertext)
+        //{
+        //    var codeToArray = code.ToCharArray();
+        //    var alphabetToArray = alphabet.ToCharArray();
+        //    //var characters = ciphertext;
+        //    var outputText = string.Empty;
+
+        //    foreach (var i in ciphertext.ToUpper())
+        //    {
+        //        if (alphabetToArray.Contains(i))
+        //        {
+        //            outputText += alphabetToArray[Array.IndexOf(codeToArray, i)];
+        //        }
+        //        else outputText += i;
+        //        //Console.WriteLine(i);
+        //    }
+
+        //    Console.WriteLine(outputText);
+        //}
     }
 }
