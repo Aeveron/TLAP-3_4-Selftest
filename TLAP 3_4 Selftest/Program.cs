aa4d6120a507069746c383aff086549e26625082
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-17 12:23:35 +0200

    x
    x

--- TLAP 3_4 Selftest/Program.cs
+++ TLAP 3_4 Selftest/Program.cs
@@ -8,15 +8,17 @@ namespace TLAP_3_4_Selftest
 
     class Program
     {
-        private static readonly Random random = new Random();
+        static readonly Random Random = new Random();
         static void Main(string[] args)
         {
-            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
-            var code = "QWERTYUIOPÅÆØLKJHGFDSAZXCVBNM";
-            var ciphertext = "Hei på deg!";
+            //string[] alphabetArr = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å" };
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var code = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+            var ciphertext = "Hei på deg";
             MainEncrypt(alphabet, code, ciphertext);
             BackToPlainText(alphabet, code, ciphertext);
             Console.WriteLine(ciphertext);
+
 
         }
 
@@ -25,20 +27,21 @@ namespace TLAP_3_4_Selftest
             var codeToArray = code.ToCharArray();
             var alphabetToArray = alphabet.ToCharArray();
             var outputText = string.Empty;
+            var characters = ciphertext;
+
 
             foreach (var i in ciphertext.ToUpper())
             {
+                var randomAlpha = Random.Next(alphabetToArray.Length, i);
                 if (alphabetToArray.Contains(i))
                 {
                     outputText += alphabetToArray[Array.IndexOf(codeToArray, i)];
                 }
-                else outputText += i;
+                else outputText += RandomNess();
             }
             Console.WriteLine(outputText);
+
         }
-
-
-
 
         static void BackToPlainText(string alphabet, string code, string ciphertext)
         {
@@ -49,15 +52,22 @@ namespace TLAP_3_4_Selftest
 
             foreach (var i in ciphertext.ToUpper())
             {
+                var randomAlpha = Random.Next(alphabetToArray.Length, i);
                 if (alphabetToArray.Contains(i))
                 {
                     outputText += alphabetToArray[Array.IndexOf(codeToArray, i)];
+
                 }
-                else outputText += i;
+                else outputText += RandomNess();
             }
             Console.WriteLine(characters);
             Console.WriteLine(outputText);
 
+        }
+
+        private static char RandomNess()
+        {
+            return (char) Random.Next('A', 'Z');
         }
     }
 }
